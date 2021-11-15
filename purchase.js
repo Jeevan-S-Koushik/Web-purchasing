b8d6c139d7f2777779f3df7b9f39a1682c11bad5
Jeevan S Koushik <78801996+Jeevan-S-Koushik@users.noreply.github.com>
2021-11-15 22:31:47 +0530

    Add files via upload

---
+++ purchase.js
@@ -0,0 +1,35 @@
+function myfunction(){
+    var a=document.getElementById("text1").value;
+    var b=document.getElementById("text2").value;
+    var c=document.getElementById("text3").value;
+    var d=document.getElementById("text4").value;
+    var e=document.getElementById("text5").value;
+    var f=document.getElementById("text6").value;
+
+    document.getElementById("area").value=a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f;
+
+    var a=document.getElementById("text1").value="";
+    var b=document.getElementById("text2").value="";
+    var c=document.getElementById("text3").value="";
+    var d=document.getElementById("text4").value="";
+    var e=document.getElementById("text5").value="";
+    var f=document.getElementById("text6").value="";
+}
+function Confirm(){
+    alert("Order Placed, Thank you for Purchasing"+"You will get further information about your Website")
+}
+function help(){
+    alert("This is the place where you can Purchase Websites "+"If in case you have any query feel free to CONTACT : 6362050849")
+}
+function fun1(){
+    alert("This Website is for Software Company "+" This Website will cost around 10,000/-")
+}
+function fun2(){
+    alert("This Website is for Agriculture / Medical Fiels  "+" This Website will cost around 20,000/-")
+}
+function fun3(){
+    alert("This Website is for School and Colleges "+" This Website will cost around 10,000/-")
+}
+function fun4(){
+    alert("This Website is for Startup Companies "+" This Website will cost around 35,000/-")
+}
